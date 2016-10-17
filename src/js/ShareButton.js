aed29611ad313bf2b078b8fadc1b2dca0dc2a05b
kazuki morita <kz.morit@gmail.com>
2016-10-18 00:34:32 +0900

    shareボタンのUI実装

--- src/js/ShareButton.js
+++ src/js/ShareButton.js
@@ -3,6 +3,7 @@
  */
 export default class ShareButton {
     constructor (btn) {
+        this.parentBtn = btn
         this.shareBtn = btn.getElementsByClassName('share')[0]
         this.socialBtns = btn.getElementsByClassName('social-btns')[0]
         this.isOpened = false
@@ -12,11 +13,22 @@ export default class ShareButton {
 
     setListener() {
         this.shareBtn.addEventListener('click', (e) => {
+
           if (this.isOpened){
+            this.parentBtn.classList.remove('open')
+            this.shareBtn.classList.remove('open')
+            this.socialBtns.classList.remove('open')
+
+            this.parentBtn.classList.add('close')
             this.shareBtn.classList.add('close')
             this.socialBtns.classList.add('close')
           }
           else {
+            this.parentBtn.classList.remove('close')
+            this.shareBtn.classList.remove('close')
+            this.socialBtns.classList.remove('close')
+
+            this.parentBtn.classList.add('open')
             this.shareBtn.classList.add('open')
             this.socialBtns.classList.add('open')
           }
