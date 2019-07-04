6bf1b21e11f17c5c247a5cb5231b065cd7db37be
kazuki morita <kz.morit@gmail.com>
2019-07-05 06:56:04 +0900

    Disable performance hints

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,34 @@
-const path = require('path')
+const path = require('path');
 
 module.exports = {
   entry: {
-    "javascripts/entry": "./src/entry.js", 
-    "javascripts/preload": "./src/preload.js",
+    'javascripts/entry': './src/entry.js',
+    'javascripts/preload': './src/preload.js',
   },
   output: {
-    path: path.resolve(__dirname, "static"),
-    filename: "[name].bundle.js"
+    path: path.resolve(__dirname, 'static'),
+    filename: '[name].bundle.js',
   },
   module: {
-      rules: [
-          {
-              parser: { amd: false },
-              include: /node_modules\/lodash\// // https://github.com/lodash/lodash/issues/3052
-          },
-          {
-              test: /\.js$/,
-              loader: "babel-loader",
-              options: {
-                  presets: ["es2015"]   
-              }
-          },
-          { test: /\.css$/, loader: "style!css" },
-          { test: /\.scss$/, loaders: ["style-loader", "css-loader", "sass-loader"] },
-          { test: /\.(png|woff|woff2|eot|ttf|svg)$/, use: 'url-loader?limit=100000' }
-      ]
+    rules: [
+      {
+        parser: {amd: false},
+        include: /node_modules\/lodash\//, // https://github.com/lodash/lodash/issues/3052
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['es2015'],
+        },
+      },
+      {test: /\.css$/, loader: 'style!css'},
+      {test: /\.scss$/, loaders: ['style-loader', 'css-loader', 'sass-loader']},
+      {test: /\.(png|woff|woff2|eot|ttf|svg)$/, use: 'url-loader?limit=100000'},
+    ],
   },
   resolve: {
-    extensions: [".js", ".json", ".jsx"]
-  }
+    extensions: ['.js', '.json', '.jsx'],
+  },
+  performance: {hints: false},
 };
