d407b4fbcc6d3c551d96f59a834c94004006d587
kazuki morita <kz.morit@gmail.com>
2019-12-02 15:30:45 +0900

    Add share button at buttom of article

--- src/entry.js
+++ src/entry.js
@@ -1,22 +1,22 @@
-import RippleEffect from "./js/RippleEffect"
-import ShareButton  from "./js/ShareButton"
-import SocialShare  from "./js/SocialShare"
-import mermaid from "mermaid"
+import RippleEffect from './js/RippleEffect';
+import ShareButton from './js/ShareButton';
+import SocialShare from './js/SocialShare';
+import mermaid from 'mermaid';
 
-let rippleBtns = document.querySelectorAll(".ripple")
-Array.from(rippleBtns).forEach((btn) => {
-    new RippleEffect(btn)
-})
+let rippleBtns = document.querySelectorAll('.ripple');
+Array.from(rippleBtns).forEach(btn => {
+  new RippleEffect(btn);
+});
 
-let shareBtns = document.querySelectorAll(".share-button")
-Array.from(shareBtns).forEach((btn) => {
-    new ShareButton(btn)
-})
+let shareBtns = document.querySelectorAll('.share-button');
+Array.from(shareBtns).forEach(btn => {
+  new ShareButton(btn);
+});
 
-let socialShare = new SocialShare()
-let socialBtns = document.querySelectorAll(".social-btns li a")
-Array.from(socialBtns).forEach((btn) => {
-    socialShare.attach(btn)
-})
+let socialShare = new SocialShare();
+let socialBtns = document.querySelectorAll('.social-button a');
+Array.from(socialBtns).forEach(btn => {
+  socialShare.attach(btn);
+});
 
-mermaid.initialize({startOnLoad:true, theme: 'forest'})
+mermaid.initialize({startOnLoad: true, theme: 'forest'});
--- src/js/SocialShare.js
+++ src/js/SocialShare.js
@@ -1,39 +1,46 @@
+export default class SocialShare {
+  coustructor() {}
 
-export default class SocialShare 
-{
-    coustructor(){
+  attach(btn) {
+    // TODO::シェア数を表示する
+    const siteUrl = btn.dataset.url;
+    const service = btn.dataset.service;
+    const title = btn.dataset.title;
+
+    var serviceUrl = '';
+    if (service == 'facebook') {
+      serviceUrl =
+        'https://facebook.com/sharer/sharer.php?u=' +
+        encodeURIComponent(siteUrl);
+    }
+    if (service == 'twitter') {
+      const message = title + ' @kz_morita';
+      serviceUrl =
+        'https://twitter.com/share?url=' +
+        encodeURIComponent(siteUrl) +
+        '&text=' +
+        encodeURIComponent(message);
+    }
+    if (service == 'pocket') {
+      serviceUrl =
+        'http://getpocket.com/edit?url=' + encodeURIComponent(siteUrl);
+    }
+    if (service == 'hatebu') {
+      serviceUrl = 'http://b.hatena.ne.jp/entry/' + siteUrl;
     }
 
-    attach(btn) {
+    btn.addEventListener('click', e => {
+      this.open(serviceUrl, service);
+      return false;
+    });
+  }
 
-        // TODO::シェア数を表示する
-        const siteUrl = btn.dataset.url
-        const service = btn.dataset.service
-        const title   = btn.dataset.title
-
-        var serviceUrl = ""
-        if (service == "facebook") {
-            serviceUrl = 'https://facebook.com/sharer/sharer.php?u=' + encodeURIComponent(siteUrl)
-        }
-        if (service == "twitter") {
-            serviceUrl = 'https://twitter.com/share?url=' + encodeURIComponent(siteUrl) + '&text=' + encodeURIComponent(title)
-        }
-        if (service == "pocket") {
-            serviceUrl = 'http://getpocket.com/edit?url=' + encodeURIComponent(siteUrl)
-        }
-        if (service == "hatebu") {
-            serviceUrl = 'http://b.hatena.ne.jp/entry/' + siteUrl
-        }
-
-        btn.addEventListener("click", (e) => {
-            this.open(serviceUrl, service)
-            return false
-        })
-    }
-
-    open(url, service)
-    {
-        window.open(url, service + 'シェア', 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width=600, height=350');
-        return false;
-    }
+  open(url, service) {
+    window.open(
+      url,
+      service + 'シェア',
+      'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width=600, height=350',
+    );
+    return false;
+  }
 }
