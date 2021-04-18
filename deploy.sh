1022068957febe7ebf648cb6678093ef42979555
kz_morita <kz.morit@gmail.com>
2021-04-18 16:44:04 +0900

    Add deploy command

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-# deploy.shが置いてあるディレクトリへ移動
 DIR=$(cd $(dirname $0);pwd)
 cd $DIR
 
+source ./.env
+
 # build
+npm run webpack
 hugo
+
+# deploy
+echo "deploying to s3 buckets";
+echo $S3_BUCKET;
+
+aws s3 sync --delete ./public s3://$S3_BUCKET --profile $AWS_PROFILE
+
+# invalidate cache
+aws cloudfront create-invalidation --distribution-id $CLOUDFRONT_DISTRIBUTION_ID --paths "/*" --profile $AWS_PROFILE
